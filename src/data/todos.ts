faa08677bd6c6efec90c443f6741e9a3698ffb97
Ctato1 <dznelashvilitato@gmail.com>
2024-02-19 19:06:45 +0400

    add todo list from data

---
+++ src/data/todos.ts
@@ -0,0 +1,19 @@
+import { Todo } from "../types/todo"
+
+export const todoData: Todo[] =[
+    {
+        id:1,
+        title:'Hello today i have a Lection',
+        completed:false,
+    },
+    {
+        id:2,
+        title:'Go to SuperMarket',
+        completed:false,
+    },
+    {
+        id:3,
+        title:'Drink some tea',
+        completed:false,
+    },
+]
