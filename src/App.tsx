152917717de32cf9601e39afda3d4f90efd5e45d
Ctato1 <dznelashvilitato@gmail.com>
2024-02-20 05:30:47 +0400

    add functionality sort and delete todos with animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import { todoData } from "./data/todos";
-import TodoItem from "./components/TodoItem";
 import AddTodoForm from "./components/AddTodoForm";
+import TodoList from "./components/TodoList";
 
 function App() {
   const [todos, setTodos] = useState(todoData)
+  const [animation, setAnimatin] = useState(Number)
 
   function setTodoCompleted(id:number,completed:boolean){
     setTodos(prev=> prev.map(item=>(item.id === id ? {...item,completed} : item)))
@@ -12,19 +13,22 @@ function App() {
   function setInputValue(title: string){
     setTodos(prev=> [{title:title,completed:false,id:Date.now()}, ...prev])
   }
+  
+  function deleteTodos(id: number){
+    setAnimatin(id)
+    setTimeout(() => {
+      setTodos(prev=> prev.filter(todo => todo.id !== id))
+      setAnimatin(0)
+    }, 1950);
+
+  }
 
   return (
     <main className="py-10  h-screen space-y-5">
       <h1 className="font-bold text-3xl text-center">Your Todos</h1>
       <div className="max-w-lg mx-auto bg-slate-200 rounded-md p-4">
       <AddTodoForm onSubmit={setInputValue}/>
-        <div className="space-y-2">
-          {
-            todos.map(todo=>(
-              <TodoItem todo={todo} onCompleteChange={setTodoCompleted}/>
-            ))
-          }
-        </div>
+       <TodoList todos={todos} onCompleteChange={setTodoCompleted} onDelete={deleteTodos} animation={animation}/>
       </div>
     </main>
   );
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,22 +1,31 @@
+import { Trash2Icon } from "lucide-react";
 import { Todo } from "../types/todo";
 interface TodoItemProps {
   todo: Todo;
   onCompleteChange: (id:number,completed:boolean)=> void;
+  onDeleteChange: (id:number)=> void;
+  animation:number;
 }
 
-export default function TodoItem({ todo,onCompleteChange }: TodoItemProps) {
+export default function TodoItem({ todo,onCompleteChange,onDeleteChange,animation }: TodoItemProps) {
 
   return (
-    <div className="space-y-2 " key={todo.id}>
-      <label className="flex w-96 gap-2 items-center border rounded-md p-3 border-gray-400 bg-white hover:bg-slate-50 cursor-pointer">
+    <div className="gap-2 flex items-center overflow-hidden">
+      <label className={`flex grow gap-2 items-center border rounded-md p-3 border-gray-400 bg-white hover:bg-slate-50 cursor-pointer ${animation === todo.id ? 'animate-pulse' : ''}`}>
         <input type="checkbox" className="scale-125" 
         checked={todo.completed}
         onChange={(e)=>onCompleteChange(todo.id,e.target.checked)}
         />
-        <span className={todo.completed ? 'line-through text-gray-300' : ''}>
+        <span className={`${todo.completed ? 'line-through text-gray-300' : ''}`}>
             {todo.title}
         </span>  
       </label>
+      <button className="p-2"
+      onClick={()=>onDeleteChange(todo.id)}
+      >
+        <Trash2Icon size={25} className="text-gray-600"/>
+      </button>
     </div>
   );
 }
+// ${animation ? 'animate-ping' : ''}
